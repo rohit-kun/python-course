824286ab668e628e941aa4d83b3808c46245125c
Rohit Kumar Rout <rohit.kumar.rout0075@gmail.com>
2026-09-25 12:55:36 +0530

    first commit

---
+++ assignment1.py
@@ -0,0 +1,3 @@
+# print your name nae 10 times using * symbol and each time it shall be on next line
+
+print(("rohit" + "\n") * 10)
---
+++ userinput.py
@@ -0,0 +1,5 @@
+# program to take input from the user
+
+print("A sample program to understand input method in python")
+name = input("Enter your name: ")
+print(f"Your name is: {name}")
